67d270ea76aa1311e3959b3185bcd9e946df3dc7
AmardeepMauryaR <amardeepmaurya9988@gmail.com>
2021-02-02 21:20:35 +0530

    url config update

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,6 @@
-export const api = 'http://localhost:2000/api';
-export const generatePublicUrl = (fileName) => {
-    return `http://localhost:2000/public/${fileName}`;
+ const baseUrl = "https://flipkart-clone-apps.herokuapp.com";
+
+ export const api = `${baseUrl}/api`;
+ export const generatePublicUrl = (fileName) => {
+    return `${baseUrl}/public/${fileName}`;
 }
